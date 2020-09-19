3ee20a5d8f845a34bdce6b3a30c16da80c4bf23f
sansaldo <root@LAPTOP-VJ2BCD67.localdomain>
2020-09-19 18:22:45 -0400

    load from cfg

--- mobilebot/mobilebot.c
+++ mobilebot/mobilebot.c
@@ -210,8 +210,8 @@ void mobilebot_controller(){
     update_now();
     read_mb_sensors();
     mb_controller_update(&mb_state, &mb_setpoints);
-    mb_motor_set(LEFT_MOTOR, mb_state->left_cmd);
-    mb_motor_set(RIGHT_MOTOR, mb_state->right_cmd);
+    mb_motor_set(LEFT_MOTOR, mb_state.left_cmd);
+    mb_motor_set(RIGHT_MOTOR, mb_state.right_cmd);
     publish_mb_msgs();
 }
 
