d2b782fe1e9aac0a408b475860cadc9f261e6ad6
sansaldo <root@LAPTOP-VJ2BCD67.localdomain>
2020-09-19 18:30:19 -0400

    load from cfg

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -77,6 +77,13 @@ int mb_load_controller_config(){
 int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){
     mb_state->left_cmd = rc_filter_march(&pid_filt_l, mb_setpoints->fwd_velocity + ((float) mb_state->left_encoder_delta)/(1.0/SAMPLE_RATE_HZ));
     mb_state->right_cmd = rc_filter_march(&pid_filt_r, mb_setpoints->fwd_velocity + ((float) mb_state->right_encoder_delta)/(1.0/SAMPLE_RATE_HZ));
+    
+    if(mb_state->left_cmd > 1){
+        rc_filter_reset(&pid_filt_l);
+    }
+    if(mb_state->right_cmd > 1){
+        rc_filter_reset(&pid_filt_r);
+    }
     return 0;
 }
 
