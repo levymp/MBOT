328a77f947f4045a73f61c3075a8076538dced8a
sansaldo <root@LAPTOP-VJ2BCD67.localdomain>
2020-09-19 18:38:20 -0400

    load from cfg

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -75,9 +75,11 @@ int mb_load_controller_config(){
 *******************************************************************************/
 
 int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){
-    mb_state->left_cmd = rc_filter_march(&pid_filt_l, mb_setpoints->fwd_velocity + ((float) mb_state->left_encoder_delta)/(1.0/SAMPLE_RATE_HZ));
-    mb_state->right_cmd = rc_filter_march(&pid_filt_r, mb_setpoints->fwd_velocity + ((float) mb_state->right_encoder_delta)/(1.0/SAMPLE_RATE_HZ));
-    
+    float vl = ((float) mb_state->left_encoder_delta * (WHEEL_DIAMETER * M_PI) / (GEAR_RATIO * ENCODER_RES))/(1.0/SAMPLE_RATE_HZ);
+    float vr = ((float) mb_state->left_encoder_delta * (WHEEL_DIAMETER * M_PI) / (GEAR_RATIO * ENCODER_RES))/(1.0/SAMPLE_RATE_HZ);
+    mb_state->left_cmd = rc_filter_march(&pid_filt_l, mb_setpoints->fwd_velocity - vl);
+    mb_state->right_cmd = rc_filter_march(&pid_filt_r, mb_setpoints->fwd_velocity - vr);
+
     if(mb_state->left_cmd > 1){
         rc_filter_reset(&pid_filt_l);
     }
