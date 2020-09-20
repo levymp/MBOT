c93e6cc1d5ef04b9e48b7160957cd19cdbf4ccaf
sansaldo <root@LAPTOP-VJ2BCD67.localdomain>
2020-09-20 14:56:41 -0400

    fixed filter

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -27,13 +27,6 @@ int mb_initialize_controller(){
 
 
 int mb_load_controller_config(){
-    char cwd[100];
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("Current working dir: %s\n", cwd);
-   } else {
-       perror("getcwd() error");
-       return 1;
-   }
    
     FILE* file = fopen(CFG_PATH, "wr");
     if (file == NULL){
@@ -82,8 +75,9 @@ int mb_load_controller_config(){
 *
 *******************************************************************************/
 
-int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){
+int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){ 
     float vl = ((float) mb_state->left_encoder_delta * (WHEEL_DIAMETER * PI) / (GEAR_RATIO * ENCODER_RES))/(1.0/SAMPLE_RATE_HZ);
+    printf("%f\n", vl);
     float vr = ((float) mb_state->left_encoder_delta * (WHEEL_DIAMETER * PI) / (GEAR_RATIO * ENCODER_RES))/(1.0/SAMPLE_RATE_HZ);
     mb_state->left_cmd = rc_filter_march(&pid_filt_l, mb_setpoints->fwd_velocity - vl);
     mb_state->right_cmd = rc_filter_march(&pid_filt_r, mb_setpoints->fwd_velocity - vr);
