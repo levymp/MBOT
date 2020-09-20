4b2d7cd58753afb6d3fe12e2dd94cd6ccc5ab772
sansaldo <root@LAPTOP-VJ2BCD67.localdomain>
2020-09-20 16:46:35 -0400

    fixed filter

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -91,7 +91,7 @@ int mb_load_controller_config(){
 int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){ 
     float vl = ((float) mb_state->left_encoder_delta * (WHEEL_DIAMETER * 3.141) / (GEAR_RATIO * ENCODER_RES))/(1.0/SAMPLE_RATE_HZ);
     float vr = ((float) mb_state->right_encoder_delta * (WHEEL_DIAMETER * 3.141) / (GEAR_RATIO * ENCODER_RES))/(1.0/SAMPLE_RATE_HZ);
-    mb_state->fwd_velocity = vl;
+    mb_state->fwd_velocity = mb_state->left_encoder_delta;
     mb_state->left_cmd = rc_filter_march(&pid_filt_l, vl - mb_setpoints->fwd_velocity);
     mb_state->right_cmd = rc_filter_march(&pid_filt_r, vr - mb_setpoints->fwd_velocity);
     return 0;
