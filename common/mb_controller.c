d4a2407dcf52eb0b02ef1a46643af38f27ce6360
sansaldo <sam@ansaldo.net>
2020-10-02 17:40:00 -0400

    Modified the velocity controller

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -228,8 +228,8 @@ int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){
     //mb_setpoints->right_velocity = (2 * mb_setpoints->fwd_velocity + WHEEL_BASE * mb_setpoints->turn_velocity) / 2;
 
     // Setpoint for each wheel with lowpass filter on the fwd velocity to prevent jerking the robot chassis
-    mb_setpoints->left_velocity = (2 * rc_filter_march(&lp_filt_l, mb_setpoints->fwd_velocity) - WHEEL_BASE * mb_setpoints->turn_velocity) / 2;
-    mb_setpoints->right_velocity = (2 * rc_filter_march(&lp_filt_r, mb_setpoints->fwd_velocity) + WHEEL_BASE * mb_setpoints->turn_velocity) / 2;
+    mb_setpoints->left_velocity = rc_filter_march(&lp_filt_l, mb_setpoints->fwd_velocity) - WHEEL_BASE * mb_setpoints->turn_velocity;
+    mb_setpoints->right_velocity = rc_filter_march(&lp_filt_r, mb_setpoints->fwd_velocity) + WHEEL_BASE * mb_setpoints->turn_velocity;
 
     // If there's a change in the fwd setpoint, reset the filters and start the wheel commands with the feed forward term
     if(fabs(mb_setpoints->fwd_velocity - mb_setpoints->old_fwd) > .2){
