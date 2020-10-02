4292175d7cc462529cce1b1e07f5d1b134cc98e5
sansaldo <sam@ansaldo.net>
2020-10-02 17:41:15 -0400

    Modified the velocity controller

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -232,7 +232,7 @@ int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){
     mb_setpoints->right_velocity = rc_filter_march(&lp_filt_r, mb_setpoints->fwd_velocity) + WHEEL_BASE * mb_setpoints->turn_velocity;
 
     // If there's a change in the fwd setpoint, reset the filters and start the wheel commands with the feed forward term
-    if(fabs(mb_setpoints->fwd_velocity - mb_setpoints->old_fwd) > .2){
+    if(fabs(mb_setpoints->fwd_velocity - mb_setpoints->old_fwd) > .2 || mb_setpoints->fwd_velocity + mb_setpoints->turn_velocity == 0){
 
         mb_controller_filter_reset();
         // Starting cmd, Feedforward*setpoint
