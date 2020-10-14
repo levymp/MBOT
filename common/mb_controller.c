cf4babb4580404883d2fd9dbbdf0ff8d76b65cf2
Hamil Shah <hami97@gmail.com>
2020-10-14 01:18:41 +0000

    Commit before switching to common mobilebot

--- common/mb_controller.c
+++ common/mb_controller.c
@@ -232,7 +232,7 @@ int mb_controller_update(mb_state_t* mb_state, mb_setpoints_t* mb_setpoints){
     //mb_setpoints->right_velocity = (2.0 * rc_filter_march(&lp_filt_r, mb_setpoints->fwd_velocity) + WHEEL_BASE * mb_setpoints->turn_velocity) / 2.0;
 
     // If there's a change in the fwd setpoint, reset the filters and start the wheel commands with the feed forward term
-    if(mb_setpoints->fwd_velocity != mb_setpoints->old_fwd){
+    if(abs(mb_setpoints->fwd_velocity - mb_setpoints->old_fwd) > 0.2){
 
        mb_setpoints->old_fwd = mb_setpoints->fwd_velocity;
        mb_controller_filter_reset();
